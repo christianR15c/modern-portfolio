a363157a0fe35738f8a35bb5fa941e8dcc37d8e1
christian habineza <habinezac15@gmail.com>
2023-02-08 16:09:06 +0200

    initial home added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import './App.css'
+import Home from './components/home/Home'
 
 import Navbar from './components/navbar/Navbar'
 
@@ -8,6 +9,9 @@ const App = () => {
     return (
         <>
             <Navbar />
+            <main className='main'>
+                <Home />
+            </main>
         </>
     )
 }
