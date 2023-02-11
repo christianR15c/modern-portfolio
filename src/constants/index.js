834cd3f6adbb03165cb6738e751907fb9fe963e2
christian habineza <habinezac15@gmail.com>
2023-02-11 16:14:39 +0200

    ft: testimonial section added

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,7 @@
+import image1 from '../assets/testimonial1.png'
+import image2 from '../assets/testimonial2.png'
+import image3 from '../assets/testimonial3.png'
+
 export const links = [
     {
         menu: 'Home',
@@ -87,5 +91,26 @@ export const qualifications = [
     {
         icon: 'uil uil-briefcase-alt',
         title: 'Experience'
+    },
+]
+
+export const testimonies = [
+    {
+        id: 1,
+        image: image1,
+        title: 'Tuyishime David',
+        description: 'Yyour attention to detail and ability to find creative solutions to complex problems never cease to impress me. Your contributions to our team have been invaluable'
+    },
+    {
+        id: 2,
+        image: image2,
+        title: 'Jules Bienvenue Himbaza',
+        description: 'I have been constantly amazed by your ability to quickly understand new technologies. Your hard work and expertise have played a huge role in the success of our projects'
+    },
+    {
+        id: 3,
+        image: image3,
+        title: 'Eric Ndekezi',
+        description: 'Our software development team has been utilizing the skills and expertise turn complex ideas into simple, user-friendly solutions that greatly improve our operations. '
     },
 ]
